0a84fb533b24d033122c55e6d81f1fb0be6bf674
Mehmet Ali Kipcak <mehmet.kipcak@gmail.com>
2015-01-28 21:08:30 +0200

    first acceptance test, first fail.

---
+++ trackman/src/main/java/com/twiw/trackman/TrackOptimizer.java
@@ -0,0 +1,9 @@
+package com.twiw.trackman;
+
+public class TrackOptimizer {
+	public void pack(String talks){
+	}
+	public String[][] getResultContainers(){
+		return null;
+	}
+}
---
+++ trackman/src/test/java/com/twiw/trackman/PendingTests.java
@@ -0,0 +1,11 @@
+package com.twiw.trackman;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ TrackOptimizerEndToEndTest.class })
+public class PendingTests {
+
+}
