786a200ebea06fc6b54519f0957d61e8c98e5997
Mehmet Ali Kipcak <mehmet.kipcak@gmail.com>
2015-01-28 23:45:01 +0200

    SessionFactory tests improved.

--- trackman/src/main/java/com/twiw/trackman/bean/SessionFactoryTests.java
+++ trackman/src/main/java/com/twiw/trackman/bean/SessionFactoryTests.java
@@ -9,13 +9,48 @@ import com.twiw.trackman.SessionFactory;
 public class SessionFactoryTests extends TestCase{
 
 	@Test
-	public void test() {
+	public void testMorningValumeIsValid() {
 		//arrange
-		SessionFactory sf = new SessionFactory();
+		SessionFactory sf = new SessionFactory(new int[]{3*60,4*60});
 		//act
 		Session sess = sf.create();
 		//assert
 		assertTrue("volume invalid",sess.getVolume() == 3*60);
 	}
-
+	@SuppressWarnings("unused")
+	@Test
+	public void testAfternoonVolumeIsValid() {
+		//arrange
+		SessionFactory sf = new SessionFactory(new int[]{3*60,4*60});
+		//act
+		Session sessMorng = sf.create();
+		Session sessAftNn = sf.create();
+		//assert
+		assertTrue("volume invalid",sessAftNn.getVolume() == 4*60);
+	}
+	@SuppressWarnings("unused")
+	@Test
+	public void testSecondDayMorningValumeIsValid() {
+		//arrange
+		SessionFactory sf = new SessionFactory(new int[]{3*60,4*60});
+		//act
+		Session sessMorng = sf.create();
+		Session sessAftNn = sf.create();
+		Session sessNextMorng = sf.create();
+		//assert
+		assertTrue("volume invalid",sessNextMorng.getVolume() == 3*60);
+	}	
+	@SuppressWarnings("unused")
+	@Test
+	public void testSecondDayAfternoonValumeIsValid() {
+		//arrange
+		SessionFactory sf = new SessionFactory(new int[]{3*60,4*60});
+		//act
+		Session sessMorng = sf.create();
+		Session sessAftNn = sf.create();
+		Session sessNextMorng = sf.create();
+		Session sessNextAftNn = sf.create();
+		//assert
+		assertTrue("volume invalid",sessNextAftNn.getVolume() == 4*60);
+	}
 }
