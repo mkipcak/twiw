5b46b9d3e7bf1ded93b42afe951f3f2679a2a857
Mehmet Ali Kipcak <mehmet.kipcak@gmail.com>
2015-01-30 00:56:57 +0200

    Talk instance instantiations tasks moved into designated methods.

--- trackman/src/main/java/com/twiw/trackman/TalkBuilder.java
+++ trackman/src/main/java/com/twiw/trackman/TalkBuilder.java
@@ -8,34 +8,45 @@ import java.util.regex.Pattern;
 import com.twiw.trackman.bean.Talk;
 
 public class TalkBuilder {
-	
+	static final String TALKTITLE_LUNCH = "Lunch";
+	static final String TALKTITLE_NETWORK = "Networking Event";
+	static final String REGULARLINE_MINUTESUFFIX = "min";
 	static final String LIGHTKEYWORD = "lightning";
 	static final int LIGHTVAL_INMIN = 5;
 
 	public Talk build(String line){
 		
-		if(line == null) {
+		if(line == null||line.trim().length() == 0) {
 			return null;
 		} 
 		else if(line.trim().endsWith(LIGHTKEYWORD)){
-			return new Talk(LIGHTVAL_INMIN);
+			return createForLightinging(line);
 		}
-		else if(line.endsWith("min")){
-			Pattern linePattern = Pattern.compile("(.*?)(\\d+)(.*)");
-			Matcher m = linePattern.matcher(line);
-			if (m.matches()) {
-			  String title = m.group(1);
-			  String minAsStr = m.group(2);
-			  int volInMinutes = Integer.parseInt(minAsStr);
-			  return new Talk(title, volInMinutes);
-			}
+		else if(line.endsWith(REGULARLINE_MINUTESUFFIX)){
+			return createForRegularLine(line);
 		}
 		return null;
+	}
+	private Talk createForRegularLine(String line){
+		Pattern linePattern = Pattern.compile("(.*?)(\\d+)(.*)");
+		Matcher m = linePattern.matcher(line);
+		if (m.matches()) {
+		  return new Talk(m.group(1).trim(), Integer.parseInt(m.group(2)), false);
+		}
+		return null;
+	}
+	private Talk createForLightinging(String line){
+		Pattern linePattern = Pattern.compile("(.*?)"+LIGHTKEYWORD);
+		Matcher m = linePattern.matcher(line);
+		if (m.matches()) {
+		  return new Talk(m.group(1).trim(),LIGHTVAL_INMIN, true);
+		}
+		return new Talk("unknown",LIGHTVAL_INMIN, true);
 	}
 	public List<Talk> buildAll(int... minutes){
 		List<Talk> l = new ArrayList<Talk>();
 		for (int min : minutes) {
-			Talk talk = build("t "+min+"min");
+			Talk talk = build("test "+min+REGULARLINE_MINUTESUFFIX);
 			if(talk != null) {
 				l.add(talk);
 			}
@@ -52,5 +63,8 @@ public class TalkBuilder {
 			}
 		}
 		return l;
+	}
+	public Talk buildNoVolume(String title) {
+		return new Talk(title, 0, false);
 	}
 }
